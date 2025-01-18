e9332f5329b9b32dd5fa86ec4ebb03387bede52c
raihahahan <mraihandev@gmail.com>
2025-01-18 15:16:58 +0800

    connect to supabase

--- repository/database.py
+++ repository/database.py
@@ -1,4 +1,4 @@
-import os
+from supabase import create_client, Client
+from globals import SUPABASE_KEY, SUPABASE_URL
 
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase_client: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
