be4f78cbb97eb9405848b27487c101f1b02139b8
ryantjy <ryantjy2002@gmail.com>
2025-01-18 17:19:02 +0800

    .

--- command_handler/start.py
+++ command_handler/start.py
@@ -2,7 +2,7 @@ from service import user_service
 from utils import logger
 
 def initialise(bot):
-    @bot.message_handler(commands=['start', 'hello'])
+    @bot.message_handler(commands=['start'])
     def send_welcome(message):
         logger.log(f"Received message: {message.text}", "START COMMAND HANDLER")
         username = message.from_user.username
